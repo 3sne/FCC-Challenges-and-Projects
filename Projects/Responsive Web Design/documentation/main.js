01d05ae59fffda5259b75cd7f55dee4fbf5171d8
Mukur Panchani <panchanimukur@gmail.com>
2018-06-12 18:17:22 +0530

    done 2 projects

---
+++ Projects/Responsive Web Design/documentation/main.js
@@ -0,0 +1,16 @@
+// document.body.style.setProperty('background', 'white');
+
+// document.getElementById('tsb').onclick = function () {
+//     console.log(document.body.style.backgroundColor);
+//     if(document.body.style.backgroundColor == 'white') {
+//         document.body.style.setProperty("--pri-color", "rgba(20, 20, 20, 0.9)");
+//         document.body.style.setProperty("--pri-comp", "rgba(230, 230, 230, 1)");
+//         document.body.style.setProperty("--font-color", "rgba(245, 245, 245, 0.9)");
+//         document.body.style.setProperty("color", "white");
+//     } else {
+//         document.body.style.setProperty("--pri-color", "white");
+//         document.body.style.setProperty("--pri-comp", "rgba(230, 230, 230, 1)");
+//         document.body.style.setProperty("--font-color", "rgba(245, 245, 245, 0.9)");
+//         document.body.style.setProperty("color", "white");
+//     }
+// };
