11a751fd9aa17ebc139f18622051b70637a4b120
Mukur Panchani <panchanimukur@gmail.com>
2018-06-13 11:26:20 +0530

    add 1 responsive wd project

---
+++ Projects/Responsive Web Design/prod-land/main.js
@@ -0,0 +1,9 @@
+//for smooth scrolling on clicking a link.
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
